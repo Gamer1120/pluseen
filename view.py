59bb964595abd9f19d3f9bd8c977d2c423e9b283
BuildTools <unconfigured@null.spigotmc.org>
2020-10-13 17:00:34 +0200

    fix

--- view.py
+++ view.py
@@ -75,4 +75,5 @@ def view(id):
     return "nah"
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=33507)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
