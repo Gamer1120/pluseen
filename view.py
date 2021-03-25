c1a8ed1c3f6fc27b6400ec30114ce62db619958c
Gamer1120 <michaelkoopmantest@gmail.com>
2021-03-25 16:06:24 +0100

    Fix some stuff that should have been reviewed

--- view.py
+++ view.py
@@ -37,10 +37,10 @@ def pluseen(id):
     if (str(id).isalnum()):
         result = '''
         <html>
-        <body>
         <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         </head>
+        <body>
         '''
         participants = controller.view(id)
         result = "<h1>Dit is de pluseen \""+str(id)+"\"!</h1><h2>Klik op je naam om je +1 toe te voegen of weg te halen!</h2><br><h2>+1:</h2>\n<br>"
